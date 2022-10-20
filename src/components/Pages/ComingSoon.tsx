50fe0d888782aea0d02b4474ac6a0cbe608fc0c5
Ludo Hund <Ludovic_hund@hotmail.fr>
2022-10-20 18:17:13 +0200

    favorites

--- src/components/Pages/ComingSoon.tsx
+++ src/components/Pages/ComingSoon.tsx
@@ -1,5 +1,4 @@
-import React, { useContext } from 'react';
-import { FavoritesContext } from '../../store/FavoritesProvider';
+import React from 'react';
 import LeftSideBar from '../LeftSideBar';
 
 // changer classe container !!!!!
@@ -7,18 +6,11 @@ import LeftSideBar from '../LeftSideBar';
 
 const ComingSoon = () => {
 
-    const favoritesContext = useContext(FavoritesContext);
-
-    console.log(favoritesContext)
 
     return (
         <div className='home__container'>
             <LeftSideBar/>
-            {favoritesContext.items.map((item)=>{
-                return(
-                    <img src={`https://image.tmdb.org/t/p/w400/${item.poster_path}`} alt="poster" />
-                )
-            })}
+           
         </div>
     );
 };
--- src/components/Pages/Favorites.tsx
+++ src/components/Pages/Favorites.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { FavoritesContext } from '../../store/FavoritesProvider';
 import LeftSideBar from '../LeftSideBar';
 // changer classe container !!!!!
 
 const Favorites = () => {
+    const favoritesContext = useContext(FavoritesContext);
+
+    console.log(favoritesContext)
+
     return (
-        <div className='home__container'> 
+        <div className='home__container'>
             <LeftSideBar/>
+            {favoritesContext.items.map((item)=>{
+                return(
+                    <img src={`https://image.tmdb.org/t/p/w400/${item.poster_path}`} alt="poster" />
+                )
+            })}
         </div>
     );
 };
